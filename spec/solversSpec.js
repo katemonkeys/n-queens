8a6b5612150e93dc7c60a823beeaab9528551df1
Kate Jenkins <catherine.ann.jenkins@gmail.com>
2013-10-03 22:48:23 -0700

    Rooks solve for unique solutions!

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -1,4 +1,4 @@
-describe("solvers", function() {
+describe('solvers', function() {
   window.displayBoard = function(){};
 
   describe('findNRooksSolution()', function(){
@@ -29,9 +29,16 @@ describe("solvers", function() {
 
     it('finds the number of valid UNIQUE solutions for n of 0-8', function(){
       _.range(0, 8).map(function(n){
-        var solutionCount = countNRooksSolutions(n);
-        var expectedSolutionCount = [1][n];
-        expect(solutionCount).to.be.equal(expectedSolutionCount);
+        var solutionCount = countNRooksUniqueSolutions(n);
+        var expectedSolutionCount = [1, 1, 1, 2, 7, 23, 115, 694][n];
+        expect(solutionCount[0]).to.be.equal(expectedSolutionCount[0]);
+        expect(solutionCount[1]).to.be.equal(expectedSolutionCount[1]);
+        expect(solutionCount[2]).to.be.equal(expectedSolutionCount[2]);
+        expect(solutionCount[3]).to.be.equal(expectedSolutionCount[3]);
+        expect(solutionCount[4]).to.be.equal(expectedSolutionCount[4]);
+        expect(solutionCount[5]).to.be.equal(expectedSolutionCount[5]);
+        expect(solutionCount[6]).to.be.equal(expectedSolutionCount[6]);
+        expect(solutionCount[7]).to.be.equal(expectedSolutionCount[7]);
       });
     });
 
@@ -54,7 +61,7 @@ describe("solvers", function() {
   describe('countNQueensSolutions()', function(){
 
     it('finds the number of valid solutions for n of 0-8', function(){
-      _.range(0, 8).map(function(n){
+      _.range(4, 8).map(function(n){
         var solutionCount = countNQueensSolutions(n);
         var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92][n];
         expect(solutionCount).to.be.equal(expectedSolutionCount);
@@ -66,9 +73,9 @@ describe("solvers", function() {
     describe('countNQueensUniqueSolutions()', function(){
 
     it('finds the number of valid UNIQUE solutions for n of 0-8', function(){
-      _.range(1,10).map(function(n){
-        var solutionCount = countNQueensSolutions(n);
-        var expectedSolutionCount = [1, 0, 0, 1, 2, 1, 6, 12, 46, 92][n];
+      _.range(0,10).map(function(n){
+        var solutionCount = countNQueensUniqueSolutions(n);
+        var expectedSolutionCount = [1, 1, 0, 0, 1, 2, 1, 6, 12, 46, 92][n];
         expect(solutionCount).to.be.equal(expectedSolutionCount);
       });
     });
--- src/solvers.js
+++ src/solvers.js
@@ -13,8 +13,6 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 window.findNRooksSolution = function(n){
-  // debugger;
-  // n = 5;
   var solutions = [];
 
   var placeRook = function (rowsLeft,piecesArr) {
@@ -57,11 +55,59 @@ window.countNRooksSolutions = function(n){
 
 // return the number of UNIQUE nxn chessboards that satisfy the problem
 window.countNRooksUniqueSolutions = function(n){
-  var temp = 1;
-  for (var i = n; i>0; i--) {
-    temp *= i;
+
+  var solutions = [];
+  var transposeArray = [];
+  var tempVar = 0;
+
+  var placeRook = function (rowsLeft,piecesArr) {
+    if (rowsLeft === 0) {
+      solutions.push(piecesArr);
+      return;
+    }
+//    var row = n - rowsLeft;
+    var unavailCol = [];
+    for (var i = 0 ; i<piecesArr.length; i++) {
+      unavailCol.push(piecesArr[i]);
+    }
+    for (var k = 0; k<n; k++) {
+      if (unavailCol.indexOf(k) === -1) {
+        var nextPiecesArr = piecesArr.concat(k);
+        placeRook(rowsLeft-1, nextPiecesArr);
+      }
+    }
+  };
+  placeRook(n,[]);
+
+//  console.log('Original array had '+solutions.length);
+
+  tempVar = 0;
+  var duplicates = {};
+  for (var p = 0; p<solutions.length; p++){
+    for (var j = 0; j<n; j++) {
+      transposeArray[solutions[p][j]] = j;
+    }
+    for (var m = p; m<solutions.length; m++) {
+      if (transposeArray.join('') === solutions[m].join('') ) {
+          duplicates[solutions[m].join('')] = true;
+      }
+    }
   }
-  return temp;
+
+  var duplArr = [];
+  for (var key in duplicates) {
+    duplArr.push(key);
+  }
+//console.log('duplicates by tranpose reduce by ' + duplArr.length);
+
+  return solutions.length - duplArr.length;
+
+//  console.log(tempArray);
+// Check:
+// 4f rotation
+// h
+// v
+
 };
 
 
@@ -70,7 +116,7 @@ window.findNQueensSolution = function(n){
 
   var solutions = [];
 
-  var placeRook = function (rowsLeft,piecesArr) {
+  var placeQueen = function (rowsLeft,piecesArr) {
     if (rowsLeft === 0) {
       solutions.push(piecesArr);
       return;
@@ -86,11 +132,11 @@ window.findNQueensSolution = function(n){
     for (var k = 0; k<n; k++) {
       if (unavailCol.indexOf(k) === -1) {
         var nextPiecesArr = piecesArr.concat(k);
-        placeRook(rowsLeft-1, nextPiecesArr);
+        placeQueen(rowsLeft-1, nextPiecesArr);
       }
     }
   };
-  placeRook(n,[]);
+  placeQueen(n,[]);
 
   var ourBoard = new Board({n:n});
   for(var r = 0; r < n; r++) {
@@ -130,7 +176,7 @@ window.countNQueensSolutions = function(n){
  
   var solutions = [];
 
-  var placeRook = function (rowsLeft,piecesArr) {
+  var placeQueen = function (rowsLeft,piecesArr) {
     if (rowsLeft === 0) {
       solutions.push(piecesArr);
       return;
@@ -146,11 +192,11 @@ window.countNQueensSolutions = function(n){
     for (var k = 0; k<n; k++) {
       if (unavailCol.indexOf(k) === -1) {
         var nextPiecesArr = piecesArr.concat(k);
-        placeRook(rowsLeft-1, nextPiecesArr);
+        placeQueen(rowsLeft-1, nextPiecesArr);
       }
     }
   };
-  placeRook(n,[]);
+  placeQueen(n,[]);
 
   // var ourBoard = new Board({n:n});
   // for(var r = 0; r < n; r++) {
@@ -160,5 +206,42 @@ window.countNQueensSolutions = function(n){
 
   var solutionCount = solutions.length;
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
+  return solutionCount;
+};
+
+window.countNQueensUniqueSolutions = function(n){
+ 
+  var solutions = [];
+
+  var placeQueen = function (rowsLeft,piecesArr) {
+    if (rowsLeft === 0) {
+      solutions.push(piecesArr);
+      return;
+    }
+    // debugger;
+    var row = n - rowsLeft;
+    var unavailCol = [];
+    for (var i = 0 ; i<piecesArr.length; i++) {
+      unavailCol.push(piecesArr[i]);
+      unavailCol.push(piecesArr[i]+(row - i));
+      unavailCol.push(piecesArr[i]-(row - i));
+    }
+    for (var k = 0; k<n; k++) {
+      if (unavailCol.indexOf(k) === -1) {
+        var nextPiecesArr = piecesArr.concat(k);
+        placeQueen(rowsLeft-1, nextPiecesArr);
+      }
+    }
+  };
+  placeQueen(n,[]);
+
+  // var ourBoard = new Board({n:n});
+  // for(var r = 0; r < n; r++) {
+  //   ourBoard.rows()[r][solutions[0][r]] = 1;
+  // }
+  // return ourBoard;
+  
+
+  var solutionCount = solutions.length;
   return solutionCount;
 };
